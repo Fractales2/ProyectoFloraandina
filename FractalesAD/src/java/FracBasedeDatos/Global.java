af3e98187996934a40701236c072f6b5627c3508
FabiCr22 <45839143+FabiCr22@users.noreply.github.com>
2019-02-08 05:40:15 -0500

    Update Global.java

--- FractalesAD/src/java/FracBasedeDatos/Global.java
+++ FractalesAD/src/java/FracBasedeDatos/Global.java
@@ -11,8 +11,8 @@ package FracBasedeDatos;
  */
 public class Global {
     public static String driverclass="org.postgresql.Driver"; /*el driver de la clase que ya esta en servicios*/
-    public static String databaseURL="jdbc:postgresql://localhost:5432/Fractales"; /*copiar la base de datos url*/
-    public static String usuarioDB="postgres"; /*usuario de la base de datos*/
-    public static String claveDB="1593748"; /*contrasena de la base de datos*/
+    public static String databaseURL="d1q6v9smhrek4g"; /*copiar la base de datos url*/
+    public static String usuarioDB="uweerwinbxhhwp"; /*usuario de la base de datos*/
+    public static String claveDB="9a3a72d021ca5b2aaf1ebba9684305839ae66a40c6b0738d3b14937ae56f478c"; /*contrasena de la base de datos*/
     
 }
